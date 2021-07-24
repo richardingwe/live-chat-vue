606a0b2b10cc4a2a9137d2f25d135f5cb8e6a44c
richardingwe <richardingwe5@gmail.com>
2021-07-24 22:08:05 +0100

    created a signup composable

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -8,22 +8,27 @@
 		/>
 		<input type="email" required placeholder="email" v-model="email" />
 		<input type="password" required placeholder="password" v-model="password" />
+		<div class="error">{{ error }}</div>
 		<button>Sign up</button>
 	</form>
 </template>
 
 <script>
 	import { ref } from 'vue';
+	import useSignup from '../composables/useSignup';
 	export default {
 		setup() {
 			// refs
 			const displayName = ref('');
 			const email = ref('');
 			const password = ref('');
-			const handleSubmit = () => {
-				console.log(displayName.value, email.value, password.value);
+			// useSignup
+			const { error, signup } = useSignup();
+			const handleSubmit = async () => {
+				await signup(email.value, password.value, displayName.value);
+				console.log('user signed up');
 			};
-			return { displayName, email, password, handleSubmit };
+			return { displayName, email, password, handleSubmit, error };
 		},
 	};
 </script>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
-import firebase from './firebase/app';
-import './firebase/firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
