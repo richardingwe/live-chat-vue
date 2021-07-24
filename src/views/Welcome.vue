7bad650e7cca13238d8d552fdba839c155e6e875
richardingwe <richardingwe5@gmail.com>
2021-07-24 21:07:44 +0100

    added styles to the login and sign up components

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,16 +1,69 @@
 <template>
 	<div class="welcome container">
-		<p>welcome</p>
+		<div v-if="showLogin">
+			<h2>Login</h2>
+			<LoginForm />
+			<p>
+				No account yet? <span @click="showLogin = false">Signup</span> instead.
+			</p>
+		</div>
+		<div v-else>
+			<h2>Sign up</h2>
+			<SignupForm />
+			<p>
+				Already registered?
+				<span @click="showLogin = true">Login</span> instead.
+			</p>
+		</div>
 	</div>
 </template>
 
 <script>
-	export default {};
+	import SignupForm from '../components/SignupForm.vue';
+	import LoginForm from '../components/LoginForm.vue';
+	import { ref } from 'vue';
+	export default {
+		components: { SignupForm, LoginForm },
+		setup() {
+			const showLogin = ref(true);
+			return { showLogin };
+		},
+	};
 </script>
 
 <style>
 	.welcome {
 		text-align: center;
 		padding: 20px 0;
+	}
+	/* form styles */
+	.welcome form {
+		width: 300px;
+		margin: 20px auto;
+	}
+	.welcome label {
+		display: block;
+		margin: 20px 0 10px;
+	}
+	.welcome input {
+		width: 100%;
+		padding: 10px;
+		border-radius: 20px;
+		border: 1px solid #eee;
+		outline: none;
+		color: #999;
+		margin: 10px auto;
+	}
+	.welcome span {
+		font-weight: bold;
+		text-decoration: underline;
+		cursor: pointer;
+	}
+	.welcome button {
+		margin: 20px auto;
+		cursor: pointer;
+	}
+	.welcome button:hover {
+		background: rgb(39, 177, 136);
 	}
 </style>
