57eb813043aa401bf477aeb6a20c85fb4470776b
richardingwe <richardingwe5@gmail.com>
2021-07-24 22:20:26 +0100

    added useLogin composable

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,21 +2,27 @@
 	<form @submit.prevent="handleSubmit">
 		<input type="email" required placeholder="email" v-model="email" />
 		<input type="password" required placeholder="password" v-model="password" />
+		<div class="error">{{ error }}</div>
 		<button>Log in</button>
 	</form>
 </template>
 
 <script>
 	import { ref } from 'vue';
+	import useLogin from '../composables/useLogin';
 	export default {
 		setup() {
 			// refs
 			const email = ref('');
 			const password = ref('');
-			const handleSubmit = () => {
-				console.log(email.value, password.value);
+			const { error, login } = useLogin();
+			const handleSubmit = async () => {
+				await login(email.value, password.value);
+				if (!error.value) {
+					console.log('user logged in');
+				}
 			};
-			return { email, password, handleSubmit };
+			return { email, password, handleSubmit, error };
 		},
 	};
 </script>
