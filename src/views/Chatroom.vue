ea20c03585db5e400408101977d6aa503171739b
richardingwe <richardingwe5@gmail.com>
2021-07-29 22:13:13 +0100

    watching the current user

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -6,8 +6,21 @@
 
 <script>
 	import Navbar from '../components/Navbar.vue';
+	import getUser from '../composables/getUser';
+	import { watch } from 'vue';
+	import { useRouter } from 'vue-router';
 	export default {
 		components: { Navbar },
+		setup() {
+			const router = useRouter();
+			const { user } = getUser();
+			// if the user value is ever null, redirect to welcome screen
+			watch(user, () => {
+				if (!user.value) {
+					router.push({ name: 'Welcome' });
+				}
+			});
+		},
 	};
 </script>
 
