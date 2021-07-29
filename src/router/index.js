0e606c7dec856e98be5d7312a73094ee871feb4a
richardingwe <richardingwe5@gmail.com>
2021-07-29 21:47:59 +0100

    added route guards for unauthorized users

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Welcome from '../views/Welcome.vue';
 import Chatroom from '../views/Chatroom.vue';
+import { projectAuth } from '../firebase/config';
+
+// auth guards
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  console.log('current user in auth guard: ', user);
+  if (!user) {
+    next({ name: 'Welcome' });
+  } else {
+    next();
+  }
+};
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'Welcome',
     component: Welcome
   },
   {
     path: '/chatroom',
     name: 'Chatroom',
-    component: Chatroom
-  },
+    component: Chatroom,
+    beforeEnter: requireAuth
+  }
 ];
 
 const router = createRouter({
